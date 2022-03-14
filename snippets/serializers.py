22a3b093c5db17ae18f59a447a3bcbf43331084f
Akifumi Maeda <akifumi_maeda@maede.co.jp>
2022-03-14 07:17:53 +0000

    Creating a serializer class

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -11,7 +11,7 @@ class SnippetSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         """
-        Create and return a new `Snippet` instance, given the  validated data.
+        Create and return a new `Snippet` instance, given the validated data.
         """
         return Snippet.objects.create(**validated_data)
 
